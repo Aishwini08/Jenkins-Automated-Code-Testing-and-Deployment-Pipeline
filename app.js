6114e22c14941206b7a607e42ee32b7d87e04164
Aishwini <aishwinipriyapillai@gmail.com>
2026-03-31 21:24:58 +0530

    Implement CI/CD pipeline with Jenkins, Docker, and PM2 for automated deployment

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 console.log("Hello from Node App");
 console.log("Creating my first Jenkins pipeline");
-console.log("This is a simple Node.js application for Jenkins pipeline demonstration");
 console.log("This application will be used to demonstrate automated code testing and deployment using Jenkins");
 console.log("This is the end of the Node.js application code");
